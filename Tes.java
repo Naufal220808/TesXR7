24d938db2af34bb0ba3839bc2d79b0e1619cb5f4
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:54:14 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,6 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Nayaka Naufal 'Aziz dari kelas X RPL 7 yang berasal dari kota Sidoarjo");
+        String a = "1";
+    }
+}
