062988b517637db6a72ecdde1ea6bf6ada8a389c
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:25 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "Naufal";
+        int usia = 15;
+        String alamat = "Jl. Danau Kerinci 4 Sawojajar";
+        double kelas = 10.7; 
+    
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Usia : " + usia + " tahun");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
