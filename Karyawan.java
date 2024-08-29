d809c009ed379ad18795b9012a45d1b87701c04f
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:42:04 +0700

    Add files via upload

---
+++ Karyawan.java
@@ -0,0 +1,58 @@
+//object class
+public class Karyawan {
+    int id;
+    String nama;
+    String unit;
+    String alamat;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    //Constructor Parameter
+    public Karyawan(int id, String nama, String unit, String alamat) {
+        //variabel = parameter
+        this.id = id;
+        this.nama = nama;
+        this.unit = unit;
+        this.alamat = alamat;
+    }
+
+    //method print
+    public void print() {
+        System.out.println("ID : "+ id);
+        System.out.println("Nama : "+ nama);
+        System.out.println("Unit : "+ unit);
+        System.out.println("Alamat : "+ alamat);
+    }
+}
+    
+
